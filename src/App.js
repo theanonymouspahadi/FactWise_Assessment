09c23a5cbc8f69a543b1f75b95afa80ca220d97f
Prajjwal Bisht <112104242+theanonymouspahadi@users.noreply.github.com>
2023-07-04 13:55:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
+import "./App.css";
+import  AccordionComponent  from "./Accordian";
+import { useState } from "react";
+import database from "./database.json";
+import { Grid, Box } from "@mui/material";
 
-import './App.css';
-import Accordian from './Accordian';
-
+import Searchbar from "./Searchbar";
 
 function App() {
+  const [data, setData] = useState(database);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    const filteredData = database.filter((item) =>
+      item.first.toLowerCase().includes(query.toLowerCase())
+    );
+    setData(filteredData);
+  };
+
+  const handleDelete = (item) => {
+    const tempID = item.id;
+    const newData = data.filter((item) => item.id !== tempID);
+    setData(newData);
+  };
+
   return (
-    <>
-      <Accordian />
-    </>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: "100vh" }}
+      spacing={2}
+    >
+      <Grid item>
+        <Box width={900}>
+          <Searchbar value={searchQuery} onChange={handleSearch} />
+          {data.map((item) => (
+            <AccordionComponent
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+              setData={setData}
+            />
+          ))}
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
